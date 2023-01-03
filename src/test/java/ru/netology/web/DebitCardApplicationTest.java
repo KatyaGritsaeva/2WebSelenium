3c6e7718b6c42475f1cc28d96380a15933a647c0
KatyaGritsaeva <gekaterina2019@gmail.com>
2023-01-03 20:05:42 +0300

    add test

---
+++ src/test/java/ru/netology/web/DebitCardApplicationTest.java
@@ -0,0 +1,43 @@
+package ru.netology.web;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class DebitCardApplicationTest {
+
+    private WebDriver driver;
+
+    @BeforeAll
+    static void setUpAll(){
+        System.setProperty("webdriver.crome.driver", "Driver/win/chromedriver.exe");
+    }
+
+    @BeforeEach
+    void setUp(){
+        driver = new ChromeDriver();
+    }
+
+//    @BeforeEach
+//    void  tearsDown(){
+//        driver.quit();
+//        driver=null;
+//    }
+
+    @Test
+    void test1(){
+        driver.get("http://localhost:9999");
+        driver.findElement(By.cssSelector("[type=\"text\"]")).sendKeys("Екатерина");
+        driver.findElement(By.cssSelector("[type=\"tel\"]")).sendKeys("+79112555555");
+        driver.findElement(By.className("checkbox__box")).click();
+        driver.findElement(By.className("button")).click();
+        String text = driver.findElement(By.className("paragraph")).getText();
+        Assertions.assertEquals("  Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.", text);
+
+    }
+
+}
