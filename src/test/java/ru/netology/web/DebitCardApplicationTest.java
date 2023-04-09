7569fdfce6d08d46523eafdadffe2eefc81adc96
KatyaGritsaeva <gekaterina2019@gmail.com>
2023-04-09 18:49:16 +0300

    first commit

--- src/test/java/ru/netology/web/DebitCardApplicationTest.java
+++ src/test/java/ru/netology/web/DebitCardApplicationTest.java
@@ -7,6 +7,7 @@ import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 
 public class DebitCardApplicationTest {
@@ -16,12 +17,21 @@ public class DebitCardApplicationTest {
     @BeforeAll
     static void setUpAll(){
         System.setProperty("webdriver.chrome.driver", "Driver/win/chromedriver.exe");
+        //WebDriverManager.chromedriver().setup();
     }
 
-    @BeforeEach
-    void setUp(){
-        driver = new ChromeDriver();
-    }
+//    @BeforeEach
+//    void setUp(){
+//        driver = new ChromeDriver();
+//    }
+@BeforeEach
+public void setUp() {
+    ChromeOptions options = new ChromeOptions();
+    options.addArguments("--disable-dev-shm-usage");
+    options.addArguments("--no-sandbox");
+    options.addArguments("--headless");
+    driver = new ChromeDriver(options);
+}
 
 //    @BeforeEach
 //    void  tearsDown(){
