5236d210f9fed016e48cf37bf066e96b2e33d93e
KatyaGritsaeva <gekaterina2019@gmail.com>
2023-05-24 23:03:17 +0300

    new selector

--- src/test/java/ru/netology/web/DebitCardApplicationTest.java
+++ src/test/java/ru/netology/web/DebitCardApplicationTest.java
@@ -39,7 +39,7 @@ public class DebitCardApplicationTest {
         driver.findElement(By.cssSelector("[type=\"tel\"]")).sendKeys("+79112555555");
         driver.findElement(By.className("checkbox__box")).click();
         driver.findElement(By.className("button")).click();
-        String text = driver.findElement(By.className("paragraph")).getText();
+        String text = driver.findElement(By.className("paragraph_theme_alfa-on-white")).getText();
         Assertions.assertEquals("  Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.", text);
 
     }
